a44aa3d584d3858d93f6465f46491ca3df15bf68
Michael <muhinmihajlo40@gmail.com>
2025-06-13 18:55:20 +0300

    Added the Delphi app with the Contact class

---
+++ DevTrustTestTask.DelphiApp/Records/contact.pas
@@ -0,0 +1,18 @@
+unit Contact;
+
+interface
+
+uses
+  Classes, SysUtils;
+
+Type
+  TContact = record
+    FirstName: string;
+    LastName: string;
+    PhoneNumber: string;
+    Email: string;
+  end;
+
+  implementation
+  end.
+
